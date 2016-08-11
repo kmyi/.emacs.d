148667362ed89576a67cd6eebcfc275fc503a645
Kwang Moo Yi <kwang.m.yi@gmail.com>
2016-08-11 09:44:55 +0200

    allow some variables to be set

--- init.el
+++ init.el
@@ -669,7 +669,17 @@ You can disable 'clean-buffer-list' by (cancel-timer
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(safe-local-variable-values
+   (quote
+    ((eval when
+	   (fboundp
+	    (quote aggressive-indent-mode))
+	   (aggressive-indent-mode -1))
+     (eval when
+	   (fboundp
+	    (quote rainbow-mode))
+	   (rainbow-mode 1))
+     (header-auto-update-enabled)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
