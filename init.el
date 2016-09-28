d507da6ad9de7be4a0069bbc20cd9d6bbf0de2af
Kwang Moo Yi <kwang.m.yi@gmail.com>
2016-09-28 18:52:49 +0200

    updates for using org mode

--- init.el
+++ init.el
@@ -276,6 +276,21 @@ You can disable 'clean-buffer-list' by (cancel-timer
       (append '(".*\.init.el.*" ".*helm.*")
 	      clean-buffer-list-kill-never-regexps-init))
 
+;; ============================================================================
+;; Org-mode
+;; ============================================================================
+
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(setq org-agenda-files '("~/Org"))
+(setq org-enforce-todo-dependencies t) 
+
+;; Priorities
+(setq org-highest-priority ?A)
+(setq org-lowest-priority ?E)
+(setq org-default-priority ?D)
 
 ;; ============================================================================
 ;; HELM
