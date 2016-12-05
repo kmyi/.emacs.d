90ae1fffb319fafbdc1c21c2063970a056dff77d
Kwang Moo Yi <kwang.m.yi@gmail.com>
2016-12-05 14:48:42 +0100

    comment line function for easy manipulation

--- init.el
+++ init.el
@@ -104,6 +104,19 @@
 ;; Dumb jump
 (dumb-jump-mode)
 
+;; comment or uncomment lines
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)
+        (next-logical-line)))
+
+(global-set-key  (kbd "C-x C-;") 'comment-or-uncomment-region-or-line)
+
 ;; Text mode and Auto Fill mode
 ;; The next two lines put Emacs into Text mode
 ;; and Auto Fill mode, and are for writers who
