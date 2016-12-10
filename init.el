4306e63b394ed66ce326afb1fcb0c69adde694ff
Kwang Moo Yi <kwang.m.yi@gmail.com>
2016-12-10 15:55:31 +0100

    bugfix for tramp

--- init.el
+++ init.el
@@ -78,6 +78,13 @@
     (global-set-key (kbd "M-p") 'mac-print-buffer))
 )
 
+;; Tramp bug fix that started to happen for no reason
+;; One guess is due to the wrong wifi configuration 
+;; https://emacs.stackexchange.com/questions/18438/emacs-suspend-at-startup-ssh-connection-issue
+(setq tramp-ssh-controlmaster-options
+      "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+(require 'tramp)
+
 ;; ============================================================================
 ;; Basic Emacs Settings
 ;; ============================================================================
