a9b08e9c92fbf0e0eab2367b7689f1afeaf06d83
Kwang Moo Yi <kwang.m.yi@gmail.com>
2017-05-27 20:10:05 +0200

    set initial window size

--- init.el
+++ init.el
@@ -149,6 +149,10 @@
   (setq mouse-sel-mode t)
 )
 
+;; Make initial window size custom
+(add-to-list 'default-frame-alist '(height . 36))
+(add-to-list 'default-frame-alist '(width . 100))
+
 ;; Enable desktop save on exit
 (require 'desktop)
 (desktop-save-mode 1)
